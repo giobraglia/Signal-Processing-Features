1d88cd0080b46dc74565c85593184aff9963bc67
Giovanni Braglia <65290924+giobraglia@users.noreply.github.com>
2020-05-13 15:59:35 -0400

    Update itc.py

--- Features/VI features/itc.py
+++ Features/VI features/itc.py
@@ -1,1 +1,32 @@
-# i
+# Current span is simply calculated as itc = Imax - Imin
+
+import numpy as np
+
+span=[]
+
+for i in np.arange(len(current)):
+    span.append(np.max(current_cycle[i])-np.min(current_cycle[i]))
+    
+span=np.array(span)
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
