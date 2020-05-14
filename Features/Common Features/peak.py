2fe5762f2172a265dfefe7e3654b404961ae4317
Giovanni Braglia <65290924+giobraglia@users.noreply.github.com>
2020-05-14 15:01:18 -0400

    Create peak.py

---
+++ Features/Common Features/peak.py
@@ -0,0 +1,11 @@
+# Calculates the peak of the power signal
+
+import numpy as np
+
+peak=[]
+for i in np.arange(len(power)):
+    peak.append(np.max(power[i,:]))
+
+peak=np.array(peak)
+
+# power : vector containings power signals
