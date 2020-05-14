089c112f135830c1ba461868beb2f43e8ac3b0c8
Giovanni Braglia <65290924+giobraglia@users.noreply.github.com>
2020-05-14 16:59:40 -0400

    Update signal_extraction.py

--- Features/signal_extraction.py
+++ Features/signal_extraction.py
@@ -5,16 +5,16 @@ import re
 import numpy as np
 import pandas as pd
 import matplotlib.pyplot as plt
-from glob import glob
 import soundfile as sf #special library to read and manipulate audio files
+from glob import glob
 
 #%%
 def loadfiles(current_audiofiles,voltage_audiofiles,c_scalef,v_scalef):
     
-    current=[]
-    voltage=[]
+    current = []
+    voltage = []
     	
-    for i in np.arange(len(current_audiofiles)): #np.arange(20):
+    for i in np.arange(len(current_audiofiles)):
         audio, freq = sf.read(current_audiofiles[i])
         current.append([audio])
 
@@ -22,16 +22,16 @@ def loadfiles(current_audiofiles,voltage_audiofiles,c_scalef,v_scalef):
         audio, freq = sf.read(voltage_audiofiles[i])
         voltage.append([audio])
     
-    current=np.array(current)
-    voltage=np.array(voltage)
+    current = np.array(current)
+    voltage = np.array(voltage)
     
-    #scale factor processing
+    # de-normalization with scale factor
         
     for i in np.arange(len(current_audiofiles)):
-        current[i,0]*=c_scalef[i]
-        voltage[i,0]*=v_scalef[i]	
+        current[i,0] *= c_scalef[i]
+        voltage[i,0] *= v_scalef[i]	
     
-    time=(np.arange(0,len(audio))/freq)
+    time = (np.arange(0,len(audio))/freq)
     
 
     return current,voltage,time,freq
@@ -48,22 +48,21 @@ def keyFunc(afilename):
 #%%
 #set directory for source files
 
-data_dir1=r"your\file\path\Current"
-current_files= sorted(glob(data_dir1+'/*.flac'), key=keyFunc)
+data_dir1 = r"your\file\path\Current"
+current_files = sorted(glob(data_dir1+'/*.flac'), key=keyFunc)
 
-data_dir2=r"your\file\path\Voltage"
-voltage_files= sorted(glob(data_dir2+'/*.flac'), key=keyFunc)
+data_dir2 = r"your\file\path\Voltage"
+voltage_files = sorted(glob(data_dir2+'/*.flac'), key=keyFunc)
 
-df=pd.read_excel(r"path\to\scalefactors.xlsx")
+df = pd.read_excel(r"path\to\scalefactors.xlsx")   # here scale factors where saved into a .xlsx file
 
 
 
 
 #%%
-#read audiofiles and create the time array
-c_scale=np.array(df.iloc[:,2],dtype='float32')
-v_scale=np.array(df.iloc[:,3],dtype='float32')
+c_scale = np.array(df.iloc[:,0],dtype='float32')      
+v_scale = np.array(df.iloc[:,1],dtype='float32')
 
-current,voltage,time, sampling_frequency=loadfiles(current_files,voltage_files,c_scale,v_scale)
+current, voltage, time, sampling_frequency = loadfiles(current_files,voltage_files,c_scale,v_scale)
 
 
