7f1486f018163cadeee376010762fee83b33d8c6
Giovanni Braglia <65290924+giobraglia@users.noreply.github.com>
2020-05-14 14:45:22 -0400

    Update md.py

--- Features/VI features/md.py
+++ Features/VI features/md.py
@@ -1,6 +1,8 @@
 # Maximum distance from the origin
 # Displays the maximum distance from a point in the trajectory and the origin
-# This feature comes from : 
+# This feature comes from : MULINARI, B. M., CAMPOS, D. P., COSTA, C. H., ANCELMO, H. C., LAZZARETTI, A. E., OROSKI, E., LIMA, C. R. E., RENAUX, D. P. B., POTTKER, F.,
+# LINHARES, R. R. "A New Set of Steady-State and Transient Features for Power Signature Analysis Based on V-I Trajectory".
+# Accepted in: IEEE PES Innovative Smart Grid Technology Latin America, 2019.
 
 import numpy as np
 
