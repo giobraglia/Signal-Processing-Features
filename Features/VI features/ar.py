80cd1ca002749dcb4c9bbc2f128b6f3f3f1ae6ee
Giovanni Braglia <65290924+giobraglia@users.noreply.github.com>
2020-05-14 14:17:49 -0400

    Create ar.py

---
+++ Features/VI features/ar.py
@@ -0,0 +1,23 @@
+# Area of the VI trajectories.
+# This feature is computed using the Shoelace's formula, used to calculate the area of 
+# polygons, implemented in the file shoelace.py .
+# It's important to say that Shoelace's formula doesn't work for polygons with a self-intercepting shape.
+# By the way, the approximation given by this formula turned out to be useful, even for self-intercepting
+# signals.
+
+# This feature come from : 
+
+
+area_=[]
+
+for i in np.arange(len(c_cycle)):
+    area_.append(polygonArea(v_cycle[i],
+                            c_cycle[i],
+                            len(c_cycle[i])))
+    
+area_=np.array(area_)
+
+
+# c_cycle : vector whose elements are periods of the current signals analysed
+# v_cycle : vector whose elements are periods of the voltage signals analysed
+# (see 
