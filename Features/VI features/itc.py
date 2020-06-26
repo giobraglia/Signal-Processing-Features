b3a0da901d4c49d953c27482935f13d7c503ecc8
Giovanni Braglia <65290924+giobraglia@users.noreply.github.com>
2020-06-26 08:54:27 -0400

    Update itc.py

--- Features/VI features/itc.py
+++ Features/VI features/itc.py
@@ -5,16 +5,31 @@
 
 import numpy as np
 
+#%%
+# itc calculated over a sigle period
+
 span = []
 
 for i in np.arange(len(current)):
-    span.append(np.max(current_cycle[i])-np.min(current_cycle[i]))
+    span.append(np.max(c_cycle[i])-np.min(c_cycle[i]))
     
 span = np.array(span)
 
+#%%
+# itc calculated over a signal containing multiple periods (to increase the robustness of the feature)
+
+span = []
+
+for i in np.arange(len(current)):
+  peakM , _ = find_peaks(current_cycle[i],distance=1400)    # here distance 1400 was settled empirically
+  peakm , _ = find_peaks(-current_cycle[i],distance=1400)
+  span.append(np.mean(current_cycle[i][peakM])-np.mean(current_cycle[i][peakm]))
+    
+span = np.array(span)
 
 # current = vector containings current signals
 # current_cycle = vector containings small portions of the signals analysed in steady-state
+# c_cycle
 # (see pick_cycle.py)
 
 
